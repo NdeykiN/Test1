31362a00cc5d977904304da4206bf9f5719ff3fb
????????????? <ndeykin@gmail.com>
2022-07-13 13:59:43 +0300

    ADD: Initial commit.

---
+++ src/ru/xperts/test1/Main.java
@@ -0,0 +1,12 @@
+package ru.xperts.test1;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        int i = 0;
+        // TODO replace with real code HERE!
+        System.out.println("Hello, World");
+    }
+}
+
