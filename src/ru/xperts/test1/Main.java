c063ff9e74bd4bd0e8b5d6354222f5e5fec5876f
????????????? <ndeykin@gmail.com>
2022-07-13 14:01:40 +0300

    UPD: Minor fix.

--- src/ru/xperts/test1/Main.java
+++ src/ru/xperts/test1/Main.java
@@ -4,8 +4,6 @@ public class Main {
 
     public static void main(String[] args)
     {
-        int i = 0;
-        // TODO replace with real code HERE!
         System.out.println("Hello, World");
     }
 }
